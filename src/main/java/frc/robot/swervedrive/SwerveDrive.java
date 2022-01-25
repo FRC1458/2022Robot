200b6813eb7f780bf6337dd3e6d10fcf589669fa
ThirtyNinene <thirtyninene@gmail.com>
2022-01-24 17:22:54 -0800

    nvm we need WheelDrive

--- src/main/java/frc/robot/swervedrive/SwerveDrive.java
+++ src/main/java/frc/robot/swervedrive/SwerveDrive.java
@@ -1,6 +1,7 @@
 package frc.robot.swervedrive;
 
 import edu.wpi.first.math.kinematics;
+import edu.wpi.first.math.geometry;
 
 public class SwerveDrive {
     
--- src/main/java/frc/robot/swervedrive/WheelDrive.java
+++ src/main/java/frc/robot/swervedrive/WheelDrive.java
@@ -6,9 +6,19 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 public class WheelDrive {
-    
+    private CANSparkMax angleMotor;
+    private CANSparkMax speedMotor;
+    private PIDController pidController;
 
     public WheelDrive (int angleMotor, int speedMotor, int encoder) {
-        
+        this.angleMotor = new CANSparkMax(angleMotor, MotorType.kBrushless);
+        this.speedMotor = new CANSparkMax(speedMotor, MotorType.kBrushless);
+
+        //Everything below this point is copypasted, need to match to our equipment
+        pidController = new PIDController (1, 0, 0, new AnalogInput(encoder), this.angleMotor);
+    
+        pidController.setOutputRange (-1, 1);
+        pidController.setContinuous ();
+        pidController.enable ();
     }
 }
