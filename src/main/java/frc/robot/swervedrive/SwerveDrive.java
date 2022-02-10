9917b8e687dafd046c732c08389f588bb91d0eb4
Skizmo42 <eman06040@icloud.com>
2022-02-09 16:44:16 -0800

    Odometry

--- src/main/java/frc/robot/swervedrive/SwerveDrive.java
+++ src/main/java/frc/robot/swervedrive/SwerveDrive.java
@@ -4,8 +4,11 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import frc.robot.RobotConstants;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 public class SwerveDrive {
     ChassisSpeeds speeds;
@@ -14,6 +17,8 @@ public class SwerveDrive {
     Wheel backLeft;
     Wheel backRight;
     SwerveDriveKinematics kinematics;
+    SwerveDriveOdometry odometry;
+    Pose2d pose;
 
     public SwerveDrive() {
         //frontRight = new Wheel(RobotConstants.frontRightAngleID, RobotConstants.frontRightSpeedID);
@@ -29,6 +34,7 @@ public class SwerveDrive {
 
         // Creating my kinematics object using the module locations
         kinematics = new SwerveDriveKinematics(frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
+        odometry = new SwerveDriveOdometry(kinematics, new Rotation2d(), new Pose2d(5.0, 13.5, new Rotation2d()));
 
         speeds = new ChassisSpeeds();
     }
@@ -46,6 +52,14 @@ public class SwerveDrive {
 
         SmartDashboard.putNumber("SpeedMotor", moduleStates[0].speedMetersPerSecond);
         SmartDashboard.putNumber("AngleMotor", moduleStates[0].angle.getDegrees());
+
+        //Set to angle that we get from the NavX
+        double angle = 0;
+
+        Rotation2d gyroAngle = Rotation2d.fromDegrees(angle);
+
+        // Update the pose
+        pose = odometry.update(gyroAngle, moduleStates[0], moduleStates[1], moduleStates[2], moduleStates[3]);
 
         frontLeft.drive(moduleStates[0]);
         //frontRight.drive(moduleStates[1]);
