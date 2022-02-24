0e95c6bfc9213d8d98f0181a8668c94f4e396a6c
WonderfulProgress <98007678+WonderfulProgress@users.noreply.github.com>
2022-02-24 15:52:18 -0800

    Update WheelWrapper.java

--- src/main/java/frc/robot/swervedrive/WheelWrapper.java
+++ src/main/java/frc/robot/swervedrive/WheelWrapper.java
@@ -1,17 +1,28 @@
 package frc.robot.swervedrive;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
 
+//Help me I'm trapped in a WheelWrapper factory.
 
 public class WheelWrapper {
     public Wheel wheel;
     
     public WheelWrapper (int angleMotorID, int speedMotorID, String wheelName) {
         wheel = new Wheel(angleMotorID, speedMotorID, wheelName);
-        
+        try{
+            wheel = new Wheel(angleMotorID, speedMotorID, wheelName);
+        }
+        catch (RuntimeException ex ) {
+            DriverStation.reportError("Error Initiating Wheel:  " + ex.getMessage(), true);
+        }
     }
 
     public void drive(SwerveModuleState state) {
         wheel.drive(state);
     }
     
+    public void zeroEncoders() {
+        wheel.zeroEncoders();
+    }
+
 }
