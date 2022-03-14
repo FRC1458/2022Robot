99b9530ef21171ef25434aced060fd666caf4a54
NeilGhose <neil@ghose.us>
2022-03-13 17:27:57 -0700

    changed getyaw to getpitch for field oriented

--- src/main/java/frc/robot/swervedrive/SwerveDrive.java
+++ src/main/java/frc/robot/swervedrive/SwerveDrive.java
@@ -52,7 +52,7 @@ public class SwerveDrive {
         speeds.omegaRadiansPerSecond = r;
 
         if (fieldOriented) {
-            speeds = ChassisSpeeds.fromFieldRelativeSpeeds(x, y, r, Rotation2d.fromDegrees(-ahrs.getYaw()));
+            speeds = ChassisSpeeds.fromFieldRelativeSpeeds(x, y, r, Rotation2d.fromDegrees(-ahrs.getPitch()));
         }
         SmartDashboard.putNumber("angle from navx", ahrs.getYaw());
 
